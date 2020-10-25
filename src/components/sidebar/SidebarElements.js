7eade517c140b1484abf089583e74fa4b18090fb
Andrew Dillon <majik895@gmail.com>
2020-10-25 10:32:20 -0400

    burger menu working

--- src/components/sidebar/SidebarElements.js
+++ src/components/sidebar/SidebarElements.js
@@ -14,8 +14,8 @@ export const SidebarContainer = styled.aside`
   top: 0;
   left: 0;
   transition: 0.3s ease-in-out;
-  opacity: ${({ isOpen }) => (!isOpen ? "100%" : "0")};
-  top: ${({ isOpen }) => (!isOpen ? "0" : "-100%")};
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
 `;
 
 export const CloseIcon = styled(FaTimes)`
@@ -84,6 +84,6 @@ export const SidebarRoute = styled(LinkR)`
 
   &:hover {
     transition: all 0.2s ease-in-out;
-    color:#FFCD58;
+    color: #ffcd58;
   }
 `;
--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import Navbar from '../components/Navbar/Navbar'
 import Sidebar from '../components/sidebar/Sidebar'
 
 const Routes = () => {
+const [isOpen, setIsOpen] = useState(false)
+
+const toggle = () => {
+    setIsOpen(!isOpen)
+}
+
+
     return (
         <Router>
-            <Sidebar />
-            <Navbar />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
         </Router>
     )
 }
