40bf06441cbe9229b964490a0f2e8ed7a5e2f805
Andrew Dillon <majik895@gmail.com>
2020-10-25 20:14:04 -0400

    hero section finished

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,44 @@
-import React from 'react'
-import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1} from './HeroElements'
-import HeroVid from '../../videos/finance.mp4'
-
-
-
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import {Button} from "../ButtonElement";
+import HeroVid from "../../videos/finance_1.mp4";
 
 const Hero = () => {
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={HeroVid} type='video/mp4' />
-            </HeroBg>
-            <HeroContent>
-                <HeroH1>Virtual BitCoin Made Easy!</HeroH1>
-            </HeroContent>
-        </HeroContainer>
-    )
-}
+  const [hover, setHover] = useState(false);
 
-export default Hero
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={HeroVid} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual BitCoin Made Easy!</HeroH1>
+        <HeroP>
+          Sign up for a new account today and recieve your first Bitcoin
+          absolutly free.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
